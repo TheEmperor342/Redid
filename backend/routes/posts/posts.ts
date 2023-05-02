e1e70103c0fb9b275649acec2ac4726075b42ccf
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2023-05-02 21:02:49 +0530

    posts: move postController and route to its own folder

--- backend/routes/posts/posts.ts
+++ backend/routes/posts/posts.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import postsController from "../controllers/postsController";
+import postsController from "../../controllers/posts/postsController";
 const router = Router();
 
 router.post("/", postsController.post);
