180b81608297f87f9bfe774d3ba3ef421810a8d6
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2023-05-02 21:08:53 +0530

    models: add guilds model

--- backend/models/index.ts
+++ backend/models/index.ts
@@ -1,5 +1,6 @@
 import Accounts from "./accounts";
 import Tokens from "./tokens";
 import Posts from "./posts";
+import Guilds from "./guilds";
 
-export { Accounts, Tokens, Posts };
+export { Accounts, Tokens, Posts, Guilds };
