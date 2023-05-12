5d9ded8ee1d54a6ff7a0649eb29b3b944c6e9fd7
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2023-05-12 20:54:08 +0530

    feat: (backend) cors

--- backend/index.ts
+++ backend/index.ts
@@ -2,12 +2,14 @@ import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import router from "./api/routes";
 import { HttpError } from "./api/utils";
+import cors from "cors";
 
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/public", express.static("public"));
 
