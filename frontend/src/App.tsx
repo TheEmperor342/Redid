c6567443ed8883adb56d6ed560c2f8aca889029e
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2023-05-14 21:41:50 +0530

    feat: (frontend) signUp, react-router, pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,16 @@
-import { useEffect, useState } from "react";
-import Card from "./components/card/card";
+import Home from "./pages/home";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
-
-interface IPost {
-	_id: string;
-	poster: string;
-	guild: string;
-	title: string;
-	content: string;
-	likes: number;
-}
-
-const API = "http://localhost:3000";
+import SignUp from "./pages/signUp";
 
 export default () => {
-	const [data, setData] = useState<IPost[]>([]);
-	const [error, setError] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
 
-	useEffect(() => {
-		fetch(`${API}/api/posts`, { mode: "cors" })
-			.then(res => {
-				if (res.ok) return res.json();
-				else throw res;
-			})
-			.then(json => {
-				setData(json.data);
-			})
-			.catch(err => {
-				console.log(err);
-				setError(true);
-			})
-			.finally(() => setIsLoading(false));
-	}, [])
-
-	return (<>
-		<Navbar />
-		<div className="content">
-			{isLoading && <h1>Loading</h1>}
-			{error && <h1>Error</h1>}
-			{data.map(el =>
-				<Card key={el._id} poster={el.poster} guild={el.guild} title={el.title} content={el.content} likes={el.likes}/>
-			)}
-		</div>
+	return (
+	<>
+		<Navbar/>
+		<Routes>
+			<Route path='/' element={<Home/>}/>
+			<Route path='/sign-up' element={<SignUp/>}/>
+		</Routes>
 	</>);
 }
