bfd1dbfd6340a31849a6343865c7484dcc0d1811
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2023-05-18 20:53:59 +0530

    feat: (backend) check if post is liked by user

--- backend/api/routes/posts.ts
+++ backend/api/routes/posts.ts
@@ -4,6 +4,7 @@ import { verifyTokenM } from "../middlewares";
 const router = Router();
 
 router.get("/", postsController.get);
+router.get("/:id/isLikedByMe", verifyTokenM, postsController.isLikedByMe);
 router.post("/", verifyTokenM, postsController.post);
 router.delete("/:postId", verifyTokenM, postsController.delete);
 router.post("/:id/like", verifyTokenM, postsController.likePost);
