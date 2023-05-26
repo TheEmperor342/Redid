3e2d81557a4ee68805891939071bbdaf42993ba7
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2023-05-26 20:44:04 +0530

    feat: (frontend) view posts on a guild

---
+++ frontend/src/pages/guild/guild.tsx
@@ -0,0 +1,56 @@
+import usePostsReducer from "@src/hooks/usePostsReducer";
+import { GenericPageProps } from "@types";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Card from "@components/card";
+import API from "@src/apiPath";
+
+const Guild: React.FC<GenericPageProps> = ({ newError }) => {
+  const { guild } = useParams();
+  const [posts, postsDispatch] = usePostsReducer([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [guildExists, setGuildExists] = useState<boolean>(true);
+
+  useEffect(() => {
+    getGuildPosts().then((json) =>
+      postsDispatch({ type: "populate", payload: json })
+    );
+  }, []);
+
+  const getGuildPosts = async () => {
+    const res = await fetch(`${API}/api/guilds/${guild}/posts`);
+    const json = await res.json();
+
+    if (!res.ok) {
+      setGuildExists(false);
+      setIsLoading(false);
+      return;
+    }
+    setIsLoading(false);
+    return json.data;
+  };
+
+  return !isLoading ? (
+    guildExists ? (
+      <div>
+        <h1>{guild}</h1>
+        {posts.map((post) => (
+          <Card
+            key={self.crypto.randomUUID()}
+            data={post}
+            newError={newError}
+            deletePost={(payload: string) => {
+              postsDispatch({ type: "delete", payload });
+            }}
+          />
+        ))}
+      </div>
+    ) : (
+      <h1> Loading</h1>
+    )
+  ) : (
+    <h1>Loading</h1>
+  );
+};
+
+export default Guild;
