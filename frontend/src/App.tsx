e01738e899a960a8bf1b96304e23a8907e52482a
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2023-05-26 20:44:04 +0530

    feat: (frontend) view posts on a guild

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Post from "@pages/post";
 import Settings from "@pages/settings";
 import User from "@pages/user/user";
 import "@src/index.css";
+import Guild from "@pages/guild/guild";
 
 const errorsReducer = (
   state: IErrorsState[],
@@ -45,6 +46,7 @@ export default () => {
         <Route path="/post" element={<Post newError={newError} />} />
         <Route path="/settings" element={<Settings newError={newError} />} />
         <Route path="/u/:username" element={<User newError={newError} />} />
+        <Route path="/g/:guild" element={<Guild newError={newError} />} />
       </Routes>
       <Errors errors={errors} remove={remove} />
     </>
