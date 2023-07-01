14ea0bbc3ac1b88d87e47de97cf820198860f8d1
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2023-07-01 21:20:12 +0530

    feat: css for `/settings`, `/u/<username>`

--- frontend/src/components/organisedPostsView/organisedPostsView.tsx
+++ frontend/src/components/organisedPostsView/organisedPostsView.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "@components/card/card";
 import { OrganisedPostsViewProps } from "@types";
+import "./style.css";
 
 const organisedPostsView: React.FC<OrganisedPostsViewProps> = ({
   posts,
