48bfbad28b55ea24ac3165fb9ba37a51bd846ee7
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2023-08-17 20:46:02 +0530

    hotfix: routing
    Switch from `BrowserRouter` to `HashRouter`

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AppWithContext } from "@src/TokenContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppWithContext />
-    </BrowserRouter>
-  </React.StrictMode>
+	<HashRouter>
+		<AppWithContext />
+	</HashRouter>
 );
