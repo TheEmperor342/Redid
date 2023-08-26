8c29dd1e1e03ee004eb61946488f20b5950312df
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2023-08-26 23:06:27 +0530

    feat: use ID to verify user; perf: useMemo for decoded tokens

--- frontend/src/pages/settings/settings.tsx
+++ frontend/src/pages/settings/settings.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GenericPageProps } from "@types";
 import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { TokenContext } from "@src/TokenContext";
@@ -8,67 +8,75 @@ import CreateGuild from "@components/newGuild";
 import "./index.css";
 
 const Settings: React.FC<GenericPageProps> = ({ newError }) => {
-  const location = useLocation();
+	const location = useLocation();
 
-  const { token, setToken } = useContext(TokenContext);
-  if (token === null)
-    return <Navigate to="/sign-in" state={{ from: location }} replace />;
+	const { token, setToken } = useContext(TokenContext);
+	if (token === null)
+		return <Navigate to="/sign-in" state={{ from: location }} replace />;
 
-  const navigate = useNavigate();
+	const [username, setUsername] = useState<string>("Loading username");
+	const navigate = useNavigate();
 
-  const tokenDecoded = JSON.parse(atob(token.split(".")[1]));
-  const username = tokenDecoded.username;
+	useEffect(() => {
+		getUsername();
+	}, []);
 
-  if (username.trim() === "" || !username) {
-    newError({
-      id: self.crypto.randomUUID(),
-      title: "Invalid token",
-      error: "Your token seems to be manipulated. Please logout and relogin.",
-    });
-    return <Navigate to="/" state={{ from: location }} replace />;
-  }
+	const getUsername = async () => {
+		const res = await fetch(`${API}/api/user`, {
+			headers: { Authorization: `Bearer ${token}` },
+		});
+		const json = await res.json();
+		if (!res.ok)
+			newError({
+				id: self.crypto.randomUUID(),
+				title: "Couldn't fetch data",
+				error: `${res.status}: ${json.message}`,
+			});
 
-  const handleDelete = async () => {
-    const res = await fetch(`${API}/api/auth/delete-user`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const json = await res.json();
-    if (!res.ok)
-      newError({
-        id: self.crypto.randomUUID(),
-        title: "Couldn't delete account",
-        error: `${res.status}: ${res.statusText}\n${JSON.stringify(json)}`,
-      });
-    setToken(null);
-    navigate("/");
-  };
-  return (
-    <div className="settingsWrapper">
-      <h1>{username}</h1>
-      <div className="settingsWrapper__grid">
-        <div>
-          <p>View your posts</p>
-          <Link to={`/u/${username}`}>
-            <button className="primary-btn">
-              <AiOutlineLink />
-            </button>
-          </Link>
-        </div>
-        <div>
-          Delete Account
-          <button className="primary-btn" onClick={handleDelete}>
-            <AiFillDelete />
-          </button>
-        </div>
-        <div className="settingsWrapper__grid__createGuild">
-          <CreateGuild newError={newError} />
-        </div>
-      </div>
-    </div>
-  );
+		else setUsername(json.username);
+	};
+
+	const handleDelete = async () => {
+		const res = await fetch(`${API}/api/auth/delete-user`, {
+			method: "DELETE",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		const json = await res.json();
+		if (!res.ok)
+			newError({
+				id: self.crypto.randomUUID(),
+				title: "Couldn't delete account",
+				error: `${res.status}: ${res.statusText}\n${JSON.stringify(json)}`,
+			});
+		setToken(null);
+		navigate("/");
+	};
+	return (
+		<div className="settingsWrapper">
+			<h1>{username}</h1>
+			<div className="settingsWrapper__grid">
+				<div>
+					<p>View your posts</p>
+					<Link to={`/u/${username}`}>
+						<button className="primary-btn">
+							<AiOutlineLink />
+						</button>
+					</Link>
+				</div>
+				<div>
+					Delete Account
+					<button className="primary-btn" onClick={handleDelete}>
+						<AiFillDelete />
+					</button>
+				</div>
+				<div className="settingsWrapper__grid__createGuild">
+					<CreateGuild newError={newError} />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Settings;
